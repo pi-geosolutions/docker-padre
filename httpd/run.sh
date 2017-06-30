69f581ccbbc9c5a51b9f9f6a56bbf20cda998f79
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2017-06-30 12:14:17 +0200

    HTTPD : new try

--- httpd/run.sh
+++ httpd/run.sh
@@ -3,6 +3,10 @@
 #create default index.html file if it does not exist
 #touch /var/www/html/index.html
 
+#Copy phppgadmin folder to /var/www/html
+cp -pr /var/www/${PHPPGADMIN_VERSION} /var/www/html/
+mv /var/www/html/${PHPPGADMIN_VERSION} /var/www/html/pgadmin
+
 #add 'padre' to list of users banned from phppgadmin
 sed -i -E "s|'pgsql', 'postgres', 'root', 'administrator'|'pgsql', 'postgres', 'root', 'padre', 'collect', 'administrator'|gm" pgadmin/classes/Misc.php
 
