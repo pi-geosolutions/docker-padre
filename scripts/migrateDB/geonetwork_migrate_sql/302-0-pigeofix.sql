ea5fccc2b382ab30e22c52f476d2a65d26047e5e
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2017-10-09 11:34:19 +0200

    [geonetwork migrate DB] fixes resource-panel-not-showing-in-mtd-edition pb

--- scripts/migrateDB/geonetwork_migrate_sql/302-0-pigeofix.sql
+++ scripts/migrateDB/geonetwork_migrate_sql/302-0-pigeofix.sql
@@ -1,1 +1,3 @@
 ALTER TABLE groups ALTER COLUMN email TYPE varchar(128);
+UPDATE Settings SET datatype = 3 WHERE name = 'map/proj4js';
+UPDATE Settings SET datatype = 3 WHERE name = 'metadata/editor/schemaConfig';
