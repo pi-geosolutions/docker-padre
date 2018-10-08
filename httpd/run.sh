76de1bd3b29cb3cb938adc8fd3e1128c524db0fb
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2018-10-08 09:44:34 +0200

    Fix rights in httpd folder

--- httpd/run.sh
+++ httpd/run.sh
@@ -7,7 +7,9 @@ touch /var/www/html/index.html
 cp -pr /var/www/phppgadmin-${PHPPGADMIN_VERSION} /var/www/html/
 mv /var/www/html/phppgadmin-${PHPPGADMIN_VERSION} /var/www/html/pgadmin
 chown -R www-data:www-data /var/www/html
-chmod -R 664 /var/www/html 
+find /var/www -type d -exec chmod 755 {} \;
+find /var/www -type f -exec chmod 644 {} \;
+
 
 #add 'padre' to list of users banned from phppgadmin
 sed -i -E "s|'pgsql', 'postgres', 'root', 'administrator'|'pgsql', 'postgres', 'root', 'padre', 'collect', 'administrator'|gm" pgadmin/classes/Misc.php
